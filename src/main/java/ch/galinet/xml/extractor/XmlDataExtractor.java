fa1e933eda2844e31fd0c9b2378e1497609a8ed5
Nils Aellen <nils.aellen@post.ch>
2019-03-15 11:13:48 +0100

    First revision published

---
+++ src/main/java/ch/galinet/xml/extractor/XmlDataExtractor.java
@@ -0,0 +1,149 @@
+package ch.galinet.xml.extractor;
+
+import org.codehaus.stax2.XMLStreamReader2;
+import org.codehaus.stax2.ri.Stax2ReaderAdapter;
+import org.codehaus.stax2.validation.XMLValidationSchema;
+import org.codehaus.stax2.validation.XMLValidationSchemaFactory;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.function.Consumer;
+
+public class XmlDataExtractor {
+    private XMLStreamReader2 reader;
+    private Unmarshaller unmarshaller;
+
+    private Class iteratorClass;
+    private String iteratorPath;
+
+    private HashMap<String, Consumer> elements = new HashMap<>();
+    private HashMap<String, Class> classes = new HashMap<>();
+
+
+    private LinkedList<String> actualParsingThroughTags = new LinkedList<>();
+
+    public XmlDataExtractor(String xmlFileLocation, URL xsdLocation, Class jaxbFactoryClass) throws XMLStreamException, JAXBException, FileNotFoundException {
+        this(new FileInputStream(xmlFileLocation), xsdLocation, jaxbFactoryClass);
+    }
+
+    public XmlDataExtractor(InputStream input, URL xsdLocation, Class jaxbFactoryClass)
+            throws XMLStreamException, JAXBException {
+
+        unmarshaller = JAXBContext.newInstance(jaxbFactoryClass).createUnmarshaller();
+
+        XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+        reader = Stax2ReaderAdapter.wrapIfNecessary(xmlInputFactory.createXMLStreamReader(input, "UTF-8"));
+        if (xsdLocation != null) {
+            XMLValidationSchemaFactory sf = XMLValidationSchemaFactory.newInstance(XMLValidationSchema.SCHEMA_ID_W3C_SCHEMA);
+            XMLValidationSchema sv = sf.createSchema(xsdLocation);
+            reader.validateAgainst(sv);
+        }
+    }
+
+    public <T> Iterable<T> process(String path, Class<T> clazz) {
+        iteratorPath = path;
+        iteratorClass = clazz;
+        return new XmlDataExtractorIterable<>();
+    }
+
+    public <T> void addElementListener(String path, Class<T> clazz, Consumer<T> consumer) {
+        elements.put(path, consumer);
+        classes.put(path, clazz);
+    }
+
+    public void process() throws JAXBException, XMLStreamException {
+        process(null);
+    }
+
+    private Object process(String stopProcessingPath) throws JAXBException, XMLStreamException {
+        while (reader.hasNext()) {
+            if (reader.getEventType() == XMLStreamConstants.START_ELEMENT) {
+                actualParsingThroughTags.add(reader.getLocalName());
+
+                String actualPath = "/" + String.join("/", actualParsingThroughTags);
+
+                boolean elementStep = elements.containsKey(actualPath);
+                boolean stopProcessingStep = actualPath.equals(stopProcessingPath);
+
+                if (elementStep || stopProcessingStep) {
+                    actualParsingThroughTags.removeLast();
+
+                    //When unmarshalling, we don't have to call the next() on reader because the unmarshaller already call it
+                    Object o = unmarshaller.unmarshal(reader, elementStep ? classes.get(actualPath) : iteratorClass).getValue();
+                    if (elementStep) {
+                        elements.get(actualPath).accept(o);
+                    }
+                    if (stopProcessingStep) {
+                        return o;
+                    }
+                } else {
+                    reader.next();
+                }
+            } else if (reader.getEventType() == XMLStreamConstants.END_ELEMENT) {
+                if (actualParsingThroughTags.getLast().equals(reader.getLocalName())) {
+                    actualParsingThroughTags.removeLast();
+                }
+                reader.next();
+            } else {
+                reader.next();
+            }
+        }
+        return null;
+    }
+
+
+    class XmlDataExtractorIterable<T> implements Iterable<T> {
+        XmlDataExtractorIterator it = new XmlDataExtractorIterator<>();
+
+        @Override
+        public Iterator<T> iterator() {
+            return it;
+        }
+    }
+
+    class XmlDataExtractorIterator<T> implements Iterator<T> {
+
+        private Boolean hasNext = null;
+        private T next = null;
+
+        public XmlDataExtractorIterator() {
+            //process until firstElement to be iterated
+            hasNext();
+        }
+
+        @Override
+        public boolean hasNext() {
+            try {
+                if (hasNext == null) {
+                    next = (T) process(iteratorPath);
+                    hasNext = (next != null);
+                }
+                return hasNext;
+            } catch (JAXBException | XMLStreamException e) {
+                throw new RuntimeException("Error while processing XMLStream", e);
+            }
+        }
+
+        @Override
+        public T next() {
+            try {
+                //ensure hasNext is called
+                hasNext();
+                return next;
+            } finally {
+                hasNext = null;
+            }
+        }
+    }
+}
---
+++ src/test/java/ch/galinet/xml/extractor/XmlDataExtractorTest.java
@@ -0,0 +1,58 @@
+package ch.galinet.xml.extractor;
+
+import ch.galinet.xmlns.example._1.Header;
+import ch.galinet.xmlns.example._1.ObjectFactory;
+import ch.galinet.xmlns.example._1.Person;
+import org.junit.Assert;
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+
+import javax.xml.bind.JAXBException;
+import javax.xml.stream.XMLStreamException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class XmlDataExtractorTest {
+
+    @Test
+    public void processFile() throws JAXBException, XMLStreamException, FileNotFoundException {
+        URL xsdLocation = this.getClass().getResource("/xsd/example.xsd");
+        Assert.assertNotNull(xsdLocation);
+
+        XmlDataExtractor extractor = new XmlDataExtractor("src/test/resources/example.xml", xsdLocation, ObjectFactory.class);
+
+        Iterable<Person> iterable = extractor.process("/delivery/persons/person", Person.class);
+
+        Flux<Person> flux = Flux.fromIterable(iterable);
+
+        Assert.assertEquals(2, flux.count().block().longValue());
+    }
+
+    @Test
+    public void processStream() throws JAXBException, XMLStreamException, FileNotFoundException {
+        FileInputStream fis = new FileInputStream("src/test/resources/example.xml");
+        XmlDataExtractor extractor = new XmlDataExtractor(fis, null, ObjectFactory.class);
+
+        Iterable<Person> iterable = extractor.process("/delivery/persons/person", Person.class);
+
+        Flux<Person> flux = Flux.fromIterable(iterable);
+
+        Assert.assertEquals(2, flux.count().block().longValue());
+    }
+
+    @Test
+    public void processPush() throws JAXBException, XMLStreamException, FileNotFoundException {
+        AtomicInteger nbHeader = new AtomicInteger(0);
+        AtomicInteger nbVoter = new AtomicInteger(0);
+        XmlDataExtractor extractor = new XmlDataExtractor("src/test/resources/example.xml", null, ObjectFactory.class);
+
+        extractor.addElementListener("/delivery/header", Header.class, h -> nbHeader.incrementAndGet());
+        extractor.addElementListener("/delivery/persons/person", Person.class, v -> nbVoter.incrementAndGet());
+        extractor.process();
+
+        Assert.assertEquals(1, nbHeader.get());
+        Assert.assertEquals(2, nbVoter.get());
+    }
+}
