e922b3fde01a7d8fd5baec10ac0490551f5e83ed
Nils Aellen <nils.aellen@post.ch>
2019-09-19 10:07:01 +0200

    add feature to be able to consume the xmlValidationProblems

---
+++ src/main/java/ch/galinet/xml/extractor/XmlValidationProblem.java
@@ -0,0 +1,41 @@
+package ch.galinet.xml.extractor;
+
+import javax.xml.stream.Location;
+
+/**
+ * @author aellenn
+ */
+public class XmlValidationProblem {
+    public final static int SEVERITY_WARNING = 1;
+    public final static int SEVERITY_ERROR = 2;
+    public final static int SEVERITY_FATAL = 3;
+
+
+    private Location location;
+    private String message;
+    private int severity;
+    private String type;
+
+    public XmlValidationProblem(Location location, String message, int severity, String type) {
+        this.location = location;
+        this.message = message;
+        this.severity = severity;
+        this.type = type;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getSeverity() {
+        return severity;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
